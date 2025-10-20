a5030a091f1f8bae3a23a8298af9a1427e8f8cd1
adityamp2008 <adityamp2008@gmail.com>
2025-10-20 13:47:21 +0700

    MASUK

--- resources/views/layouts/add.blade.php
+++ resources/views/layouts/add.blade.php
@@ -97,50 +97,53 @@
           <!--end::Brand Link-->
         </div>
         <div class="sidebar-wrapper">
-          <nav class="mt-2">
-            <!--begin::Sidebar Menu-->
-            <ul
-              class="nav sidebar-menu flex-column"
-              data-lte-toggle="treeview"
-              role="navigation"
-              aria-label="Main navigation"
-              data-accordion="false"
-              id="navigation"
-            >
-              <li class="nav-item menu-open">
-                <a href="#" class="nav-link active">
-                  <i class="nav-icon bi bi-speedometer"></i>
-                  <p>
-                    Dashboard
-                    <i class="nav-arrow bi bi-chevron-right"></i>
-                  </p>
-                </a>
-                <ul class="nav nav-treeview">
-                  <li class="nav-item">
-                    <a href="./index.html" class="nav-link active">
-                      <i class="nav-icon bi bi-circle"></i>
-                      <p>Dashboard v1</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="./index2.html" class="nav-link">
-                      <i class="nav-icon bi bi-circle"></i>
-                      <p>Dashboard v2</p>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="./index3.html" class="nav-link">
-                      <i class="nav-icon bi bi-circle"></i>
-                      <p>Dashboard v3</p>
-                    </a>
-                  </li>
-                </ul>
-              </li>
+ <nav class="mt-2">
+  <ul class="nav sidebar-menu flex-column"
+      data-lte-toggle="treeview"
+      role="navigation"
+      aria-label="Main navigation"
+      data-accordion="false"
+      id="navigation">
 
+    <!-- Dashboard Group -->
+    <li class="nav-item {{ request()->is('dashboard*') ? 'menu-open' : '' }}">
+      <a href="#" class="nav-link {{ request()->is('admin.dashboard*') ? 'active' : '' }}">
+        <i class="nav-icon bi bi-speedometer"></i>
+        <p>
+          Dashboard
+          <i class="nav-arrow bi bi-chevron-right"></i>
+        </p>
+      </a>
 
-            </ul>
-            <!--end::Sidebar Menu-->
-          </nav>
+      <ul class="nav nav-treeview">
+        <li class="nav-item">
+          <a href="{{ route('admin.dashboard') }}"
+             class="nav-link {{ request()->is('dashboard') ? 'active' : '' }}">
+            <i class="nav-icon bi bi-circle"></i>
+            <p>Dashboard</p>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a href="{{ route('atk.index') }}"
+             class="nav-link {{ request()->is('dashboard/atk') ? 'active' : '' }}">
+            <i class="nav-icon bi bi-circle"></i>
+            <p>ATK</p>
+          </a>
+        </li>
+
+        {{-- <li class="nav-item">
+          <a href="{{ route('dashboard.v3') }}"
+             class="nav-link {{ request()->is('dashboard/v3') ? 'active' : '' }}">
+            <i class="nav-icon bi bi-circle"></i>
+            <p>Dashboard v3</p>
+          </a>
+        </li> --}}
+      </ul>
+    </li>
+  </ul>
+</nav>
+
         </div>
         <!--end::Sidebar Wrapper-->
       </aside>
